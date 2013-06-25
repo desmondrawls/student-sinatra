bcfa9955c721288ba86ca461471599a171d60193
Desmond Rawls <captaingrover@gmail.com>
2013-06-25 13:41:35 -0400

    add capistrano and build scraper with sinatra routes

--- app.rb
+++ app.rb
@@ -1,10 +1,17 @@
 require 'sinatra/base'
+require_relative 'scraper'
 
 # Why is it a good idea to wrap our App class in a module?
 module StudentSite
   class App < Sinatra::Base
-    get '/' do
-      "hello world!"
+    get '/main' do
+      @students = Student.all
+      erb :main 
+    end
+
+    get '/students/:last_name' do
+      @student = Student.find_by_last_name(params[:last_name])
+      erb :student
     end
   end
 end
